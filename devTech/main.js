2961f89a1cc50c718795b7d1dedc23f7e0fc0aa5
bogdana03 <bogdana.sorinescu@ha.ax>
2022-09-29 22:24:07 +0300

    uppdate sticy navbar

--- devTech/main.js
+++ devTech/main.js
@@ -6,4 +6,9 @@ let navbar= $(".navbar");
 $(window).scroll(function(){
     let oTop = $(".section-2").offset().top-window.innerHeight;
     console.log(oTop);
+    if($(window).scrollTop()>oTop){
+       navbar.addClass("sticky");
+    }else{
+     navbar.removeClass("sticky");
+    }
 });
