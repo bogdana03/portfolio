584f578c2377181e1cd1091cbbe567ade88e221d
bogdana03 <bogdana.sorinescu@ha.ax>
2022-10-02 22:30:14 +0300

    uppdate animated cards in main.js

--- devTech/main.js
+++ devTech/main.js
@@ -16,17 +16,27 @@ $(window).scroll(function(){
 /**Counter Animation */
 
 let nCount = function(selector){
-    $(selector).each(function(){
-       $(this).animate({
-         Counter:$(this).text()
-       },{
-          duration: 4000,
-          easing: "swing",
-          step: function(value){
-            $(this).text(Math.ceil(value))
-          }
-        }
-       );
-      });
-      };
+  $(selector).each(function(){
+    
+    $(this).animate({
+      Counter:$(this).text()
+    },{
+      duration:4000,
+      easing:"swing",
+      step:function(value){
+        $(this).text(Math.ceil(value));
+      }
+     }
+    );
+  });
+};
+
+let a = 0;
+$(window).scroll(function(){
+  let oTop = $(".numbers").offset().top-window.innerHeight;
+  if(a == 0 && $(window).scrollTop() >= oTop){
+    a++;
+    nCount(".rectang>h1");
+  }
+})
     
