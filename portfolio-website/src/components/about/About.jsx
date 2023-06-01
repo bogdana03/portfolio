26fd5b6642cca681c430592f4ccfada26134ab39
bogdana03 <bogdana.sorinescu@ha.ax>
2023-06-01 17:34:01 +0300

    portfolioweb
    first commit

---
+++ portfolio-website/src/components/about/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './about.css'
+
+
+const About = () => {
+  return (
+    <div>
+      <p>about</p>
+    </div>
+  )
+}
+
+export default About
---
+++ portfolio-website/src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './testimonials.css'
+
+const testimonials = () => {
+  return (
+    <div>
+     <p>Testimonials</p> 
+    </div>
+  )
+}
+
+export default testimonials
