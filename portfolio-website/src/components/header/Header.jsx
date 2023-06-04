d89bbdb0c2133708973f1bd547745151ca656a00
bogdana03 <bogdana.sorinescu@ha.ax>
2023-06-04 21:48:59 +0300

    create HeaderSocial file

--- portfolio-website/src/components/header/Header.jsx
+++ portfolio-website/src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
 
 const Header = () => {
   return (
@@ -8,9 +10,16 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Bogdana Sorinescu</h1>
          <h5 className="text-light">UI/UX, Frontend Developer</h5>
+         <CTA />
+
+         <div className="me">
+           <img src={ME} alt="me" />
+         </div>
+
+         <a href="#contact" className='scroll_down'>Scroll Down</a>
       </div>
     </header>
   )
-}
+} 
 
 export default Header
