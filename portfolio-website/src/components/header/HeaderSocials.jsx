0c0c692fc397df18e3a576d150d8c0908ff7c6fe
bogdana03 <bogdana.sorinescu@ha.ax>
2023-06-05 20:44:05 +0300

    addead socials media icons

---
+++ portfolio-website/src/components/header/HeaderSocials.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {FaFigma} from 'react-icons/fa'
+
+
+const HeaderSocials = () => {
+  return (
+    <div className='header_socials'>
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><BsLinkedin /></a>
+          <a href="https://github.com.com" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+            <a href="https://figma.com" target="_blank" rel="noopener noreferrer"><FaFigma /></a>
+        
+    </div>
+  )
+}
+
+export default HeaderSocials
