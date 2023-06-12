336e308824573fb4dc8e60d5aa5452c6952f3c56
bogdana03 <bogdana.sorinescu@ha.ax>
2023-06-12 14:50:19 +0300

    Update About.jsx
    about import icons

--- portfolio-website/src/components/about/About.jsx
+++ portfolio-website/src/components/about/About.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/me.jpg'
+import {AiOutlineProject} from 'react-icons/ai'
 
 
 const About = () => {
   return (
    <section id='about'>
     <h2>About Me</h2>
-
+ 
     <div className="container about_container">
       <div className="about_me">
         <div className="about_me-image">
-         <img src="../../asse" alt="" /> 
+         <img src={ME} alt="About Image" /> 
         </div>
-
       </div>
 
-      <div className="about_content"></div>
+     <div className="about_content">
+       <div className="about_cards">
+          <article className='about_card'>
+            <AiOutlineProject className='about_icon'/>
+            <h5>Projects</h5>
+            <small>Completed</small>
+          </article>
+
+          <article className='about_card'>
+            <AiOutlineProject className='about_icon'/>
+            <h5>Projects</h5>
+            <small>Completed</small>
+          </article>
+        </div>   
+      </div>
     </div>
    </section>
   )
