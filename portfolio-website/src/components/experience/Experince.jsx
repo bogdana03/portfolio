1d85b8f50257942d403d7035c0ae7d4eaa78bdac
bogdana03 <bogdana.sorinescu@ha.ax>
2023-06-15 16:21:14 +0300

    develop experince file

--- portfolio-website/src/components/experience/Experince.jsx
+++ portfolio-website/src/components/experience/Experince.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experince = () => {
   return (
@@ -8,10 +9,38 @@ const Experince = () => {
 
     <div className="container experience_container">
       <div className="experience_frontend">
+        <h3>Frontend Development</h3>
+        <div className="exeperince_content">
+          <article className='experience_details'>
+            <BsPatchCheckFill />
+            <h4>HTML</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+          <article className='experience_details'>
+            <BsPatchCheckFill />
+            <h4>CSS</h4>
+            <small className='text-light'>Intermediate</small>
+          </article>
+          <article className='experience_details'>
+            <BsPatchCheckFill />
+            <h4>Bootstrap</h4>
+            <small className='text-light'>Intermediate</small>
+          </article>
+          <article className='experience_details'>
+            <BsPatchCheckFill />
+            <h4>JavaScript</h4>
+            <small className='text-light'>Intermediate</small>
+          </article> 
+          <article className='experience_details'>
+            <BsPatchCheckFill />
+            <h4>React</h4>
+            <small className='text-light'>Beginner</small>
+          </article>
+        </div>
 
       </div>
       <div className="experience_ux_ui">
-        
+
       </div>
     </div>
    </section>
