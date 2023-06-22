1f1e105edbf4811e65a4fcba9d70275f5dae02fc
bogdana03 <bogdana.sorinescu@ha.ax>
2023-06-22 16:17:57 +0300

    started to develop services flie

--- portfolio-website/src/components/services/Services.jsx
+++ portfolio-website/src/components/services/Services.jsx
@@ -3,9 +3,22 @@ import './services.css'
 
 const Services = () => {
   return (
-    <div>
-      <p>Services</p>
+   <section id='services'>
+    <h2>Wath I Offer</h2>
+
+    <div className='container services_container'>
+      <aticle className="service">
+        <div className="service_head">
+          <h3>UI/UX Design</h3>
+        </div>
+
+        <ul className='service_list'>
+          <li></li>
+        </ul>
+      </aticle>
     </div>
+
+   </section>
   )
 }
 
